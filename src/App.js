09eac688f0f7e571746c740b1066f10180eacd92
Alejandro Parra <48602979+ajparrah@users.noreply.github.com>
2021-03-31 11:52:27 -0400

    Revert "Login And Register (#4)" (#5)
    This reverts commit 8f11d623921af4e40a815e7696fa454f0d9028f9.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Provider } from 'react-redux';
-import store from './redux/store/store';
-import AppRouter from './routers/AppRouter';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <Provider store={store}>
-      <CssBaseline />
-      <AppRouter />
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
