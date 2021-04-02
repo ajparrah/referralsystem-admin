ecde37c882c01013c6c40cee0ef515c49194d941
Alejandro Parra <alejandro_parra_15@hotmail.com>
2021-04-02 14:22:12 -0400

    fix(Project): No login, signUp - Follow the specs

--- src/api/generalAPI.js
+++ src/api/generalAPI.js
@@ -8,32 +8,5 @@ export const fetchNoToken = (endpoint, data, method = 'GET') => {
     : fetch(url, {
         method,
         headers: API_HEADER,
-        body: JSON.stringify(data),
       });
-};
-
-export const fetchToken = (endpoint, JWT, data, method = 'GET') => {
-  const url = `${API_URL}${endpoint}`;
-  if(method === 'GET') {
-    return fetch(url);
-  } else {
-    if(data) {
-      return fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json',
-          token: JWT,
-        },
-        body: JSON.stringify(data),
-      });
-    } else {
-      return fetch(url, {
-        method,
-        headers: {
-          'Content-Type': 'application/json',
-          token: JWT,
-        },
-      });
-    }
-  }
 };
